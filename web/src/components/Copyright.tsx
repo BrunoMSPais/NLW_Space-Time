6f3e0cd27ae2aacc97c1bd08cc7a0c4b6091bbe0
BrunoMSPais <bpais@outlook.pt>
2023-05-19 15:42:06 +0100

    setup github login (web)

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+// type Props = {}
+
+function Copyright(/* {}: Props */) {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da{' '}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://rocketseat.com.br"
+        className="underline transition-colors hover:text-gray-100"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
+
+export default Copyright
