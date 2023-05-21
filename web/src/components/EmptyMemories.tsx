5ef732adb014e321e985342e43b0c8c408152a48
BrunoMSPais <bpais@outlook.pt>
2023-05-22 00:28:00 +0100

    connected memories page to backend and set up it's layout (web)

--- web/src/components/EmptyMemories.tsx
+++ web/src/components/EmptyMemories.tsx
@@ -2,7 +2,7 @@
 
 function EmptyMemories(/* {}: Props */) {
   return (
-    <div className="flex flex-1 items-center justify-center">
+    <div className="flex flex-1 items-center justify-center p-16">
       <p className="w-[360px] text-center leading-relaxed">
         Você ainda não registou nenhuma lembrança, comece a{' '}
         <a href="" className="underline transition-colors hover:text-gray-50">
